4caeab5c0f15efddb7595a4f7ba2fca1a6c94d91
joel <joel.joka@epitech.eu>
2023-06-02 11:39:39 +0200

    [UPD] socket.io/ : putting all function into a other file for better understanding

---
+++ socket.io/dataHandling.js
@@ -0,0 +1,109 @@
+
+let serverDB = {
+    users : {},
+    messages : [],
+    rooms : ["general"],
+    id : 0
+  }
+
+function rename(socket, name) {
+    if (serverDB.users[socket.id] === undefined) {
+        return;
+    }
+    serverDB.users[socket.id].name = name;
+    for (let i = 0; i < serverDB.users[socket.id].rooms.length; i++)
+      socket.to(serverDB.users[socket.id].rooms[i]).emit("rename");
+    return `rename "${name}" : success`;
+}
+
+function leave(socket, room ) {
+    if (serverDB.rooms.indexOf(room) !== -1) {
+        socket.leave(room);
+        serverDB.users[socket.id].rooms.splice(serverDB.users[socket.id].rooms.indexOf(room), 1);
+        serverDB.users[socket.id].active = "general";
+        console.log("leave", serverDB.users[socket.id].rooms)
+        return `leave "${room}" : success`;
+    }
+    return `leave "${room}" : error in the syntax or server does not exist please try again`;
+}
+
+function checkUser(socket) {
+    if (serverDB.users[socket.id] === undefined) {
+        delete serverDB.users[socket.id]
+        return;
+    }
+    console.log(serverDB.users[socket.id], "received")
+}
+
+function creation(socket) {
+    if (serverDB.users[socket.id] === undefined) {
+        //sers[socket.id] = {name : `user${i}`, room: "general"};
+        serverDB.users[socket.id] = {name : `user${serverDB.id}`, rooms: ["general"], active: "general" };
+        socket.join("general");
+        console.log("user connected", serverDB.users[socket.id], )
+        socket.emit("your id", { name : serverDB.users[socket.id].name, id : socket.id , room : serverDB.users[socket.id].room, msg : serverDB.messages});
+        serverDB.id++;
+    }
+}
+
+function createRooms( socket, room) {
+    if (serverDB.rooms.indexOf(room) === -1) {
+      console.log("createRooms", room)
+      //rooms.push(room)
+      serverDB.rooms.push(room)
+      serverDB.users[socket.id].active = room;
+      serverDB.users[socket.id].rooms.push(room);
+      socket.join(room);
+      return `creation of "${room}" : success`;
+    }
+    return "error in the syntax or server already exist please try again";
+  }
+  
+  function getServer(socket) {
+    return serverDB.users[socket.id].rooms;
+  }
+
+  function deleteRooms(room) {
+    if (serverDB.rooms.indexOf(room) !== -1) {
+      console.log("deleteRooms", room)
+      //rooms.splice(rooms.indexOf(room), 1)
+      serverDB.rooms.splice(serverDB[rooms].indexOf(room), 1)
+      return `deletion of "${room }" success`;
+    }
+    return `deletion of "${room }" error in the syntax or else please try again`;
+  }
+  
+  function join(socket, room) {
+    if (serverDB.rooms.indexOf(room) === -1) {
+      return `join "${room}" : error in the syntax or server does not exist please try again`;
+    }
+    if (serverDB.users[socket.id].active === room) {
+      return `join "${room}" : error you are already in this server`;
+    }
+    serverDB.users[socket.id].rooms.push(room);
+    serverDB.users[socket.id].active = room;
+    socket.join(room);
+  
+    console.log("join", rooms)
+    return `join "${room}" : success`;
+  }
+  
+  function getsUsers(userS) {
+    let usersInRoom = [];
+    for (let user in serverDB.users) {
+      if (serverDB.users[user].active === serverDB.users[userS].active) {
+        usersInRoom.push(serverDB.users[user].name)
+      }
+    }
+    console.log("getsUsers", usersInRoom)
+    return usersInRoom;
+  }
+  
+  
+  function sendMsg(msg, userS) {
+    console.log("sendMsg", msg, userS)
+    serverDB.messages.push({user : serverDB.users[socket.id].name, msg:body})
+    io.to(serverDB.users[userS].active).emit("message", { user : serverDB.users[userS].name, msg:msg })
+  }
+
+module.exports = { createRooms, deleteRooms, join, getsUsers, sendMsg, creation, checkUser, leave, rename, getServer };
--- socket.io/index.js
+++ socket.io/index.js
@@ -6,17 +6,12 @@ const server = http.createServer(app);
 const socketServ = require("socket.io");
 const io = socketServ(server,{cors: {origin: '*',methods: ['GET', 'POST'],}});
 const  { connectionToDb } = require('./db.js');
+const  { createRooms, deleteRooms, join, getsUsers, sendMsg, creation, checkUser, leave, rename, getServer } = require ('./dataHandling.js');
 //const c = require("config");
 let users = {};
-let i = 0;
 let db;
 let rooms = ["general"];
-let serverDB = {
-  users : {},
-  messages : [],
-  rooms : ["general"],
-  id : 0
-}
+
 
 connectionToDb().then((res) => {
     db = res;
@@ -25,91 +20,26 @@ connectionToDb().then((res) => {
     console.log(err)
 })
 
-function createRooms(room) {
-  if (rooms.indexOf(room) === -1) {
-    console.log("createRooms", room)
-    //rooms.push(room)
-    serverDB[rooms].push(room)
-    return `creation of "${room}" : success`;
-  }
-  return "error in the syntax or server already exist please try again";
-}
-
-function deleteRooms(room) {
-  if (server.dbrooms.indexOf(room) !== -1) {
-    console.log("deleteRooms", room)
-    //rooms.splice(rooms.indexOf(room), 1)
-    serverDB.rooms.splice(serverDB[rooms].indexOf(room), 1)
-    return `deletion of "${room }" success`;
-  }
-  return `deletion of "${room }" error in the syntax or else please try again`;
-}
-
-function join(userS, room) {
-  if (serverDB.rooms.indexOf(room) === -1) {
-    return `join "${room}" : error in the syntax or server does not exist please try again`;
-  }
-  if (users[userS].room === room) {
-    return `join "${room}" : error you are already in this server`;
-  }
-  users[userS].room = room;
-
-  console.log("join", rooms)
-  return `join "${room}" : success`;
-}
-
-function getsUsers(userS) {
-  let usersInRoom = [];
-  for (let user in serverDB.users) {
-    if (serverDB.users[user].room === serverDB.users[userS].room) {
-      usersInRoom.push(serverDB.users[user].name)
-    }
-  }
-  console.log("getsUsers", usersInRoom)
-  return usersInRoom;
-}
-
-
-
-
-
-function sendMsg(msg, userS) {
-  console.log("sendMsg", msg, userS)
-      io.to(serverDB.users[userS].room).emit("message", { user : serverDB.users[userS].name, msg:msg })
-}
 
 
 io.on("connection", socket => {
-    if (users[socket.id] === undefined) {
-        //sers[socket.id] = {name : `user${i}`, room: "general"};
-        serverDB.users[socket.id] = {name : `user${i}`, room: "general"};
-        socket.join("general");
-        console.log("user connected", serverDB.users[socket.id], )
-        socket.emit("your id", { name : serverDB.users[socket.id].name, id : socket.id , room : serverDB.users[socket.id].room, msg : serverDB.messages});
-        i++;
-    }
+    creation(socket)
     socket.on("received", () => {
-        if (users[socket.id] === undefined) {
-            delete users[socket.id]
-            return;
-        }
-        console.log(users[socket.id], "received")
+       checkUser(socket)
     })
 
     socket.on("/list", body => {
+      socket.emit("list", getServer(socket) )
       console.log("list", rooms)
-      socket.emit("list", rooms)
     })
 
     socket.on("/leave", body => {
-      console.log("leave", rooms)
-      users[socket.id].room = "general";
-      socket.emit("leave", "back to general")
+      socket.emit("leave", leave(socket, body.post))
     })
 
     socket.on("/join", body => {
       console.log("join", body)
-      socket.emit("join", join(socket.id, body.post))
+      socket.emit("join", join(socket, body.post))
     })
 
     socket.on("/users", body => {
@@ -120,7 +50,7 @@ io.on("connection", socket => {
     socket.on("/create", body => {
       //console.log("create", body)
       //console.log("rooms", rooms)
-      socket.emit("create", createRooms(body.post))
+      socket.emit("create", createRooms(socket, body.post))
     })
 
     socket.on("/delete", body => {
@@ -131,13 +61,12 @@ io.on("connection", socket => {
 
     socket.on("send message", body => {
       //console.log("message", body, users[socket.id])
-      serverDB.messages.push({user : serverDB.users[socket.id].name, msg:body})
       sendMsg(body, socket.id)
         //io.to.() emit("message", { user : users[socket.id], msg:body })
     })
     socket.on("rename", name => {
       console.log("rename", name)
-      socket.broadcast.emit("rename", name)
+      rename(socket, name)
 })
     socket.on("disconnect", () => {
       console.log("user disconnected", serverDB.users[socket.id].name)
